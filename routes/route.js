1d569281c5a50e7c2ba6ac333c5e89d5051df07a
aliezaheer <alizaheer67@gmail.com>
2023-01-19 20:54:28 +0500

    populate user data

--- routes/route.js
+++ routes/route.js
@@ -4,5 +4,6 @@ const userController = require("../controller/UserController");
 const router = express.Router();
 
 router.post("/add", userController.addUser);
+router.get("/users-list", userController.getUsers);
 
 module.exports = router;
